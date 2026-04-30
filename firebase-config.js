1ab385d9eca6e515a926baea3915e92b6fcb7000
chemistry7ter <chemistry7t@gmail.com>
2026-04-30 16:06:47 +0300

    туц_кудшіу

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Змініть малі літери на ВЕЛИКІ ось тут:
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBlFTwBRk7K_5AFUt-jdui6SIP_b4afjmg",
+    authDomain: "chemistry-a1d3e.firebaseapp.com",
+    databaseURL: "https://chemistry-a1d3e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chemistry-a1d3e",
+    storageBucket: "chemistry-a1d3e.firebasestorage.app",
+    messagingSenderId: "659739623170",
+    appId: "1:659739623170:web:59708aa93897016b32cc1b"
+};
+
+const APP_URL = window.location.origin + window.location.pathname.replace(/[^/]*$/, '');
